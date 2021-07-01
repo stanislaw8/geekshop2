7f7da80f56c8731d0c3b8db2a9d8ab948cf5ac63
stanislaw8 <pavlovinthesky@gmail.com>
2021-07-02 00:36:54 +0300

    email sending, context processor for basket

--- users/models.py
+++ users/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils.timezone import now
+from datetime import timedelta
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
+
+    age = models.PositiveIntegerField(verbose_name='Возраст', default=18)
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
+    
+    def is_activation_key_expired(self):
+        if now() <= self.activation_key_expires:
+            return False
+        else:
+            return True
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path
-
-from users.views import UserLoginView, UserRegistrationView, UserLogoutView, UserProfileView
+from django.urls import path, re_path
+import users.views as users
+from users.views import UserLoginView, UserLogoutView, UserProfileView, send_verify_mail, register
 from django.contrib.auth.decorators import login_required
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
-    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('register/', register, name='register'),
+
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    re_path(r'^verify/(?P<email>.+)/(?P<activation_key>\w+)/$', users.verify, name='verify'),
 ]
